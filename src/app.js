a52b4468b199977b436a8ba067356230485d6ad2
wihctoh <rivertoxi911@gmail.com>
2023-09-23 23:39:04 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const apiRouter = require("./controllers/api.controller");
 
 const app = express();
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 
 app.use("/user", apiRouter);
