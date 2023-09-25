935a7bb36b2da14a39aba06b39e84de87f87534c
wihctoh <rivertoxi911@gmail.com>
2023-09-25 19:31:36 +0300

    add hew page

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -1,8 +1,8 @@
 import style from "./Header.module.scss";
 import { Link } from "react-router-dom";
 
-const Header = () => {
-  return (
+const Header = ({ login }) => {
+  return !login ? (
     <div>
       <div className={style.headerWrapper}>
         <div className={style.headerNav}>
@@ -13,6 +13,14 @@ const Header = () => {
           <div className={style.signUpBtn}>
             <Link to={"/sign_up"}>Sign Up</Link>
           </div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <div className={style.headerWrapper}>
+      <div className={style.headerNav}>
+        <div className={style.signInBtn}>
+          <Link to={"/sign_in"}>Log Out</Link>
         </div>
       </div>
     </div>
--- client/src/Pages/HomePage/HomePage.jsx
+++ client/src/Pages/HomePage/HomePage.jsx
@@ -1,9 +1,20 @@
 import Header from "../../Components/Header";
+import style from "./HomePage.module.scss";
 
 const HomePage = () => {
   return (
     <div>
       <Header />
+
+      <div className={style.homeWrapper}>
+        <div className={style.titleImg}></div>
+
+        <h1>Woohoo!</h1>
+        <p>
+          You have successfully logged into the system, and a token has been assigned to you. To log
+          out, please click on Sign Out.
+        </p>
+      </div>
     </div>
   );
 };
